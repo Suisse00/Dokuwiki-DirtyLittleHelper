859f6499bb6f5e8da0f022d7e800e43e739bd473
KalleAPunkt <82371167+KalleAPunkt@users.noreply.github.com>
2021-04-29 11:54:47 +0200

    Add files via upload

---
+++ mermaid/editor/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset='utf8'>
+	<meta name='viewport' content='width=device-width'>
+
+	<title>DLH - Mermaid live editor</title>
+
+	<link rel='stylesheet' href='styles.css'>
+	<link rel='stylesheet' href='bundle.css'>
+	<link rel="stylesheet" href="fontawesome.all.min.css">
+
+<script>
+function ga(){
+	return true;
+}
+</script>
+<style>
+.myInlineDecoration {
+	/* cursor: pointer; */
+	/* text-decoration: underline; */
+	font-weight: bold;
+	font-style: oblique;
+	background: #ffcccc;
+}
+#dgraph-div {
+	display: none;
+}
+</style>
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+  </head>
+<body>
+	<script src='bundle.js'></script>
+</body>
+</html>
