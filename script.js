70fa660db64219c03de694f188fe15850fd60c2a
KalleAPunkt <82371167+KalleAPunkt@users.noreply.github.com>
2021-05-05 10:05:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,60 @@
-if(JSINFO && !JSINFO['dlh']['overlay']) {
-    jQuery( document ).ready(function() {
-      jQuery('#dirtylittlehelper_widget').draggable();
-   });
-}
- var theUserposition= {'x':200,'y':200,'position':'absolute'};
 
-
-jQuery( document ).ready(function() {
-    jQuery( "#dirtylittlehelper_overlay" ).draggable({
-        drag: function(event,ui){
-        var position = jQuery(this).position();
-        theUserposition.y = position.top;
-         theUserposition.x =position.left;
-        },
-        stop: function(){
-        var position = jQuery(this).position();
-        theUserposition.y = position.top;
-         theUserposition.x =position.left;
-         jQuery(this).css('left',position.left);
-         jQuery(this).css('top',position.top);
-        },
-    });
-
-});
-
-jQuery(window).on("load", function(){
-                pleft = 'auto';
-                ptop = '40px' ;
-	        pright = '40px';
-	        pwidth = '500px';
-	        pheight = '450px';
-                ptype='fixed' ;
-            jQuery("#dirtylittlehelper_overlay" ).css({top: ptop, right: pright,  left: pleft, position:ptype, width: pwidth, height: pheight });
-
-
-});
-
-
+/* EDITOR BUTTON */
 
 function addBtnActionDirtylittlehelper($btn, props, edid) {
-    $btn.click(function() {
-        //jQuery('#dirtylittlehelper_overlay').toggle();
-    dlh_lets_be_dirty();
-        	return true;
-    });
-
+	$btn.click(function() {
+	dlh_lets_be_dirty();
+		return true;
+	});
 }
+
+
+
+
+/* OVERLAY */
+
+if(JSINFO && JSINFO['dlh']['overlay']) {
+
+	var dlh_theUserposition= {'x':200,'y':200,'position':'absolute'};
+
+	jQuery( document ).ready(function() {
+		jQuery( "#dirtylittlehelper_overlay" ).draggable({
+			drag: function(event,ui){
+			var position = jQuery(this).position();
+			dlh_theUserposition.y = position.top;
+			dlh_theUserposition.x =position.left;
+			},
+			stop: function(){
+			var position = jQuery(this).position();
+			dlh_theUserposition.y = position.top;
+			dlh_theUserposition.x =position.left;
+			jQuery(this).css('left',position.left);
+			jQuery(this).css('top',position.top);
+			},
+		});
+	});
+
+	jQuery(window).on("load", function(){
+		pleft = 'auto';
+		ptop = '40px' ;
+		pright = '40px';
+		pwidth = '500px';
+		pheight = '450px';
+		ptype= 'fixed' ;
+		jQuery("#dirtylittlehelper_overlay" ).css({top: ptop, right: pright,  left: pleft, position:ptype, width: pwidth, height: pheight });
+	});
+
+} //if overlay
+
+
+
+
 
 
 
 
 
 function dlh_timeid(dlh_tenthofsec = false){
-
 
     if(dlh_tenthofsec === false){
         var dlh_arr_replace =[ 'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'k' , 'm' , 'n' , 'p' , 'r' , 's' , 't' , 'u' , 'v' , 'w'  ];
@@ -71,33 +73,23 @@ function dlh_timeid(dlh_tenthofsec = false){
     var dlh_secYear = dlh_dateYear.getTime()/dlh_div_seconds|0;
 
     var dlh_sec = dlh_secNow - dlh_secYear;
-
     var dlh_rest = dlh_sec;
-
     var dlh_expo = 5;
-
     var dlh_return = 'x';
-
     var dlh_div = 0;
-
     var dlh_floor = 0;
 
     while ( dlh_rest > 0 && dlh_expo > 0 ) {
 
-        dlh_div = dlh_base**dlh_expo;
-
+		dlh_div = dlh_base**dlh_expo;
         dlh_floor = Math.floor( dlh_rest / dlh_div );
-
         dlh_return = dlh_return + dlh_arr_replace[dlh_floor];
-
         dlh_rest = dlh_rest % dlh_div;
-
         dlh_expo--;
 
-    }//while
+	}//while
 
     dlh_return = dlh_return + dlh_arr_replace[dlh_rest];
-
     dlh_return = dlh_return.replace("x","");
 
     return ((''+dlh_dateNow.getFullYear()).substr(-2))+ dlh_return
@@ -111,15 +103,14 @@ function dlh_renew_timeid(dlh_target_id){
     var dlh_target_obj = document.getElementById(dlh_target_id);
 
     if(dlh_target_obj){
-
-        dlh_target_obj.value = dlh_timeid();
+       dlh_target_obj.value = dlh_timeid();
         return true;
-
     }
 
     return false;
 
 }//dlh_renew_timeid
+
 
 
 function dlh_copyme(dlh_source_id){
@@ -131,72 +122,15 @@ function dlh_copyme(dlh_source_id){
         dlh_source_obj.select();
         dlh_source_obj.setSelectionRange(0, dlh_value.length); /* For mobile devices */
         document.execCommand("copy");
-		
-		console.log('copied to clipboard: '.dlh_value);
 
-    }else{
-      alert("ERROR javascript dlh_copyme - source not found:" + dlh_source_id);
+		console.log('SUCCESS javascript dlh_copyme - copied to clipboard: '.dlh_value);
+
+	}else{
+      console.log("ERROR javascript dlh_copyme - source not found:" + dlh_source_id);
     }
 
-}//function dlh_copyme
+} //function dlh_copyme
 
-
-
-
-
-function dlh_copypaste(dlh_source_id , dlh_target_id){
-    var dlh_source_obj = document.getElementById(dlh_source_id);
-    if(dlh_source_obj){
-        var dlh_value = dlh_source_obj.value;
-
-        dlh_insertAtCursor(dlh_target_id, dlh_value)
-
-    }else{
-      alert("ERROR javascript dlh_copyme - source not found:" + dlh_source_id);
-    }
-
-}//function dlh_copyme
-
-
-
-
-function dlh_insertAtCursor(dlh_target_id, dlh_value) {
-    var dlh_target_obj = document.getElementById(dlh_target_id);
-
-    if( dlh_target_obj ) {
-        //IE support
-        if (document.selection) {
-            dlh_target_obj.focus();
-            dlh_sel = document.selection.createRange();
-            dlh_sel.text = dlh_value;
-        }
-        // Microsoft Edge
-        else if(window.navigator.userAgent.indexOf("Edge") > -1) {
-          var dlh_startPos = dlh_target_obj.selectionStart;
-          var dlh_endPos = dlh_target_obj.selectionEnd;
-
-          dlh_target_obj.value = dlh_target_obj.value.substring(0, dlh_startPos)+ dlh_value
-                 + dlh_target_obj.value.substring(dlh_endPos, dlh_target_obj.value.length);
-
-          var pos = dlh_startPos + dlh_value.length;
-          dlh_target_obj.focus();
-          dlh_target_obj.setSelectionRange(pos, pos);
-        }
-        //MOZILLA and others
-        else if (dlh_target_obj.selectionStart || dlh_target_obj.selectionStart == '0') {
-            var dlh_startPos = dlh_target_obj.selectionStart;
-            var dlh_endPos = dlh_target_obj.selectionEnd;
-            dlh_target_obj.value = dlh_target_obj.value.substring(0, dlh_startPos)
-                + dlh_value
-                + dlh_target_obj.value.substring(dlh_endPos, dlh_target_obj.value.length);
-            dlh_target_obj.selectionStart = dlh_startPos;
-        } else {
-            dlh_target_obj.value += dlh_value;
-        }
-    }else{
-        alert("ERROR javascript dlh_insertAtCursor - target not found:" + dlh_target_id);
-    }
-}//function dlh_insertAtCursor
 
 
 
@@ -214,8 +148,7 @@ function dlh_hack_the_style(){
     'overflow':' auto', 
     'display':' block', 
     'padding-right':' 10px', 
-    'width':jQuery('#dokuwiki__site').width()+'px', 
-    'max-width':' 250%' });
+    'max-width':'1300px' });
 
   /* ADVANCED CONFIG */
   if( jQuery('#plugin_advanced_config').length ){
@@ -254,40 +187,41 @@ function dlh_select(){
 
 function dlh_lets_be_dirty( dlh_dirty_i = 0){
 
-  var dlh_console = false;
+	var dlh_console = false;
 
-  if( dlh_console ) console.log('lets become dirty');
+	if( dlh_console ) console.log('lets become dirty');
 
-  //dlh has to wait till the toolbar is finished...
+	//dlh has to wait till the toolbar is finished...
     if( dlh_console ) console.log( dlh_dirty_i );
-    var dlh_dirty_toolbar, toolbar_dlh;
+
+	var dlh_dirty_toolbar, toolbar_dlh;
 
     dlh_dirty_toolbar = document.getElementById('tool__bar');
 
     if( dlh_dirty_toolbar) {
 
-    dlh_dirty_toolbar_dlh = document.getElementById('tool__bar').innerHTML.indexOf('dirtylittlehelper_ovl_sprite.png')>-1;
+		dlh_dirty_toolbar_dlh = document.getElementById('tool__bar').innerHTML.indexOf('dirtylittlehelper_ovl_sprite.png')>-1;
 
-    while( dlh_dirty_i < 10 ){
-      ++dlh_dirty_i;
-      dlh_dirty_toolbar_dlh = document.getElementById('tool__bar').innerHTML.indexOf('dirtylittlehelper_ovl_sprite.png')>-1;
-      if( dlh_dirty_toolbar_dlh === true){
-        i=99999;
-      }else{
-        if( dlh_console ) console.log('not ready: ' + dlh_dirty_i );
-        if( dlh_console ) console.log(dlh_dirty_toolbar_dlh);
-        setTimeout(dlh_lets_be_dirty, 100, dlh_dirty_i);
-        return;
-      }
+		while( dlh_dirty_i < 10 ){
+			++dlh_dirty_i;
+			dlh_dirty_toolbar_dlh = document.getElementById('tool__bar').innerHTML.indexOf('dirtylittlehelper_ovl_sprite.png')>-1;
 
-    }
+			if( dlh_dirty_toolbar_dlh === true){
+				i=99999;
+			}else{
+				if( dlh_console ) console.log('not ready: ' + dlh_dirty_i );
+				if( dlh_console ) console.log(dlh_dirty_toolbar_dlh);
+				setTimeout(dlh_lets_be_dirty, 100, dlh_dirty_i);
+				return;
+			}
+		}
 
-    if( dlh_dirty_toolbar_dlh === true){
-      if( dlh_console ) console.log('ready:' + dlh_dirty_i );
-    }else{
-      if( dlh_console ) console.log('NOT WORKING YET: '+ dlh_dirty_i);
-      return;
-    }
+		if( dlh_dirty_toolbar_dlh === true){
+			if( dlh_console ) console.log('ready:' + dlh_dirty_i );
+		}else{
+			if( dlh_console ) console.log('NOT WORKING YET: '+ dlh_dirty_i);
+			return;
+		}
 
     }//if toolbar
     else{
@@ -297,96 +231,86 @@ function dlh_lets_be_dirty( dlh_dirty_i = 0){
 
 
 
+	//some styling
+	jQuery('button.toolbutton').css({'height':'32px','width':'32px'});
+	jQuery('#dokuwiki__site').css( {'max-width':'1700px','width':'' } );
+	jQuery('.preview').css({'width':'900px','border':'1px solid #AAAAAA','resize':'both','display':'block','overflow':'scroll'});
+
+	var i = 0;
 
 
+	if( jQuery.data(document.body).dlh_count ){
+		//blabla
+	}else{
+		jQuery.data(document.body, 'dlh_count', 1);
+	}
 
-//some styling
-  jQuery('button.toolbutton').css({'height':'32px','width':'32px'});
-  jQuery('#dokuwiki__site').css( {'max-width':'1700px','width':'' } );
-  jQuery('.preview').css({'width':'900px','border':'1px solid #AAAAAA','resize':'both','display':'block','overflow':'scroll'});
-
-  var i = 0;
-
-//  if( jQuery.data(document.body, 'dlh_count', 'done')
-
-  if( jQuery.data(document.body).dlh_count ){
-    //blabla
-  }else{
-    jQuery.data(document.body, 'dlh_count', 1);
-  }
-
-  jQuery.data(document.body, 'dlh_count', jQuery.data(document.body).dlh_count + 1 );
+	jQuery.data(document.body, 'dlh_count', jQuery.data(document.body).dlh_count + 1 );
 
 
-  if( jQuery.data(document.body).dlh_count > 10 ){
-      console.log('TOO OFTEN EXITING - FOREVER');
-      return false;
-  }
+	if( jQuery.data(document.body).dlh_count > 10 ){
+		console.log('TOO OFTEN EXITING - FOREVER');
+		return false;
+	}
 
+	if( jQuery.data(document.body).dlh_ini === 'done' ){
+		if( dlh_console ) console.log('already done');
+	
+	}else{
 
+		jQuery('#wiki__text').css('height',window.innerHeight-400 + 'px');
 
-  if( jQuery.data(document.body).dlh_ini === 'done' ){
-    if( dlh_console ) console.log('already done');
-  }else{
-    jQuery('#wiki__text').css('height',window.innerHeight-400 + 'px');
+		//walk the form...
+		i = 0;
+		var dlh_labels = [];
+		var dlh_ids = [];
+		var dlh_fields = [];
+		var this_label = false;
+		var this_id = false;
 
+		var this_comeback = false;
 
-    //walk the form...
-    i = 0;
-    var dlh_labels = [];
-    var dlh_ids = [];
-    var dlh_fields = [];
-    var this_label = false;
-    var this_id = false;
+		jQuery('#dirtylittlehelper_overlay fieldset').contents().each(
+			function(){
 
-    var this_comeback = false;
+				if( dlh_console ) {
+					console.log( this );
+					console.log('----');
+					console.log('LABEL');
+					console.log( jQuery(this).contents().filter('label').text() );
+					console.log('id');
+					console.log( jQuery(this).contents().filter('span.input').contents().filter('input').attr('id') );
+					console.log('----');
+				}//log?
 
-    jQuery('#dirtylittlehelper_overlay fieldset').contents().each(
-      function(){
+				this_label = jQuery(this).contents().filter('label').text();
+				this_id = jQuery(this).contents().filter('span.input').contents().filter('input').attr('id');
 
-        if( dlh_console ) {
-          console.log( this );
-          console.log('----');
-          console.log('LABEL');
-          console.log( jQuery(this).contents().filter('label').text() );
-          console.log('id');
-          console.log( jQuery(this).contents().filter('span.input').contents().filter('input').attr('id') );
-          console.log('----');
-        }//1==2?
+				if( this_label && this_id && jQuery(this).prop('tagName') == 'DIV'){
 
-        this_label = jQuery(this).contents().filter('label').text();
-        this_id = jQuery(this).contents().filter('span.input').contents().filter('input').attr('id');
+					//if the input fields arent ready we have to try again later!
+					if( jQuery(this).contents().filter('span.input').contents().filter('input').data('uiAutocomplete') === undefined){
 
+						if( dlh_console ) console.log( jQuery(this).contents().filter('span.input').contents().filter('input').data() );
+						if( dlh_console ) console.log( jQuery.data(document.body).dlh_ini) ;
+						if( dlh_console ) console.log('come back later');
+						this_comeback = true;
 
+					}else{
 
-        if( this_label && this_id && jQuery(this).prop('tagName') == 'DIV'){
+						dlh_labels.push(this_label);
+						dlh_ids.push(this_id.replace('.','\\.') );
+						jQuery(this).attr('id','dlh_dirty_form_'+i);
+						dlh_fields.push('dlh_dirty_form_'+i);
+					}
+				}//found something
 
-          //if the input fields arent ready we have to try again later!
-          if( jQuery(this).contents().filter('span.input').contents().filter('input').data('uiAutocomplete') === undefined){
-              if( dlh_console ) console.log( jQuery(this).contents().filter('span.input').contents().filter('input').data() );
-              if( dlh_console ) console.log( jQuery.data(document.body).dlh_ini) ;
-              if( dlh_console ) console.log('come back later');
-              this_comeback = true;
-          }else{
+				++i;
+				this_label = false;
+				this_id = false;
 
-              dlh_labels.push(this_label);
-
-              dlh_ids.push(this_id.replace('.','\\.') );
-
-              jQuery(this).attr('id','dlh_dirty_form_'+i);
-
-              dlh_fields.push('dlh_dirty_form_'+i);
-
-          }
-
-        }//found something
-
-        ++i;
-        this_label = false;
-        this_id = false;
-
-      }//function each in ....
-    );//jQuery('#dirtylittlehelper_overlay fieldset').contents().each(
+			}//function each in ....
+		);//jQuery('#dirtylittlehelper_overlay fieldset').contents().each(
 
     if( this_comeback === true ){
         if( dlh_console ) console.log('exiting');
@@ -643,12 +567,10 @@ function dlh_fullcreen_edit(){
 		jQuery('#dokuwiki__content').css({'position':'fixed','top':'10px','left':'10px','right':'10px','bottom':'10px','overflow':'scroll'});
 		jQuery.data(document.body, 'dlh_full', true );
 	}
-
-		
-	
-	
 	
 } // function dlh_fullcreen_edit
+
+
 		
 //THE TOP NEW PAGE MENU IN ADMIN MENU
 
@@ -695,7 +617,7 @@ function dlh_call_top_struct(){
 function dlh_hack_the_top(){
     if( document.getElementById('dlh_topmenu_page') !== undefined ){
 	    
-        if( jQuery('#dlh_top_struct div.field input.struct_page').data('uiAutocomplete') === undefined){
+        if( jQuery('#dlh_top_struct') !== undefined && jQuery('#dlh_top_struct div.field input.struct_page').data('uiAutocomplete') === undefined){
 		setTimeout(dlh_hack_the_top, 250, 111);
 		return false;
 	}
@@ -703,17 +625,20 @@ function dlh_hack_the_top(){
 	jQuery('body').css({'margin-top':'30px'});
 	jQuery('#dokuwiki__usertools').css({'margin-top':'30px'});
 
-	jQuery('#dlh_top_struct div.field span.label').css({'display':'none'});
+	if( jQuery('#dlh_top_struct') !== undefined){
+
+		jQuery('#dlh_top_struct div.field span.label').css({'display':'none'});
         jQuery('#dlh_top_struct div.field input.struct_page').prop('title','STRUCT SEARCH');
 
-	jQuery.data(document.body, 'dlh_top_struct_id', jQuery('#dlh_top_struct div.field input.struct_page').attr('id') );
+		jQuery.data(document.body, 'dlh_top_struct_id', jQuery('#dlh_top_struct div.field input.struct_page').attr('id') );
 
-	dlh_objectValueSet( jQuery.data(document.body).dlh_top_struct_id , JSINFO['namespace']);
+		dlh_objectValueSet( jQuery.data(document.body).dlh_top_struct_id , JSINFO['namespace']);
 
-	jQuery('#dlh_top_all').append( jQuery('#dlh_top_struct div.field') ); 
+		jQuery('#dlh_top_all').append( jQuery('#dlh_top_struct div.field') ); 
 
-	jQuery('#dlh_top_struct').css({'display':'none'});
-
+		jQuery('#dlh_top_struct').css({'display':'none'});
+	}
+	
 	jQuery('#top_button_a').css({'display':'none'});
 	
     }
@@ -725,8 +650,9 @@ function dlh_hack_the_top(){
 
 document.addEventListener("DOMContentLoaded", function(event) { 
 
-dlh_objectValueSet( 'dlh_top_newpage', '[[' + JSINFO['namespace']);
 dlh_renew_timeid('dlh_top_id');
+
+dlh_objectValueSet( 'dlh_top_newpage', '[[' + JSINFO['namespace']);
 
 jQuery(function () {
     $editor = jQuery('#dlh_top_newpage');
