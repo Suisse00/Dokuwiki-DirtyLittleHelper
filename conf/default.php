a1a08eb2ce0e883ea4886fc97f6fc75346b49c18
KalleAPunkt <82371167+KalleAPunkt@users.noreply.github.com>
2022-03-24 11:16:52 +0100

    +iphelper

--- conf/default.php
+++ conf/default.php
@@ -34,3 +34,44 @@ $conf['top_dlhid_active'] = '1';
 $conf['top_adm_active'] = '1';
 $conf['top_adm_link_text'] = '&#8734 hidden area';
 $conf['top_adm_link_wikiid'] = 'secretnamespace:';
+
+
+
+/* IP HELPER */
+
+$conf['iphelper_subnetcalculatortarget']    = '_blank';
+$conf['iphelper_subnetcalculator']    = 'https://www.tunnelsup.com/subnet-calculator/?ip=%ip%';
+$conf['iphelper_tool1urltarget']    = '_self';
+$conf['iphelper_tool1name']    = 'RDP starten (erfordert Windows mit Anpassungen)';
+$conf['iphelper_tool1url']    = 'rdp:%ip%';
+$conf['iphelper_tool2urltarget']    = '_blank';
+$conf['iphelper_tool2name']    = 'https://%ip%:4444';
+$conf['iphelper_tool2url']    = 'https://%ip%:4444';
+$conf['iphelper_tool3urltarget']    = '_self';
+$conf['iphelper_tool3name']    = 'Ping (erfordert Windows mit Anpassungen)';
+$conf['iphelper_tool3url']    = 'ping:%ip%';
+$conf['iphelper_tool4urltarget']    = '_blank';
+$conf['iphelper_tool4name']    = 'Ping (Ping.eu)';
+$conf['iphelper_tool4url']    = 'http://ping.eu/ping/?host=%ip%';
+$conf['iphelper_tool5urltarget']    = '_blank';
+$conf['iphelper_tool5name']    = 'Robtex IP Lookup';
+$conf['iphelper_tool5url']    = 'https://www.robtex.com/ip-lookup/%ip%';
+$conf['iphelper_tool6urltarget']    = '_blank';
+$conf['iphelper_tool6name']    = 'Google Search';
+$conf['iphelper_tool6url']    = 'https://www.google.de/?q=%ip%';
+$conf['iphelper_tool7urltarget']    = '_blank';
+$conf['iphelper_tool7name']    = 'http://%ip%';
+$conf['iphelper_tool7url']    = 'http://%ip%';
+$conf['iphelper_tool8urltarget']    = '_blank';
+$conf['iphelper_tool8name']    = 'https://%ip%';
+$conf['iphelper_tool8url']    = 'https://%ip%';
+$conf['iphelper_tool9urltarget']    = '_blank';
+$conf['iphelper_tool9name']    = 'ftp://%ip%';
+$conf['iphelper_tool9url']    = 'ftp://%ip%';
+$conf['iphelper_tool10urltarget']    = '_blank';
+$conf['iphelper_tool10name']    = 'ssh://%ip% (erfordert Windows mit Anpassungen)';
+$conf['iphelper_tool10url']    = 'ssh://%ip%';
+$conf['iphelper_rawtools']    = '<a href="https://whois.domaintools.com/%ip%" target="_blank">whois.domaintools.com/%ip%</a><br>
+<a href="https://www.shodan.io/search?query=%ip%" target="_blank">https://www.shodan.io/search?query=%ip%</a><br>
+<a href="https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a%ip%&run=toolpage" target="_blank">https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a%ip%&run=toolpage</a><br>
+';  
