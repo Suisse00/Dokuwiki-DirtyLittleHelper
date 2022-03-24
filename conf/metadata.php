914516527325dd9041644b595b6a1461704d24ea
KalleAPunkt <82371167+KalleAPunkt@users.noreply.github.com>
2022-03-24 11:18:00 +0100

    +iphelper

--- conf/metadata.php
+++ conf/metadata.php
@@ -32,3 +32,40 @@ $meta['top_dlhid_active'] = array('onoff');
 $meta['top_adm_active'] = array('onoff');
 $meta['top_adm_link_text'] = array('string');
 $meta['top_adm_link_wikiid'] = array('string');
+
+
+/* IP HELPER */
+
+$meta['iphelper_subnetcalculatortarget']   = array('multichoice','_choices' => array('_self','_blank','_parent','_top'));
+$meta['iphelper_subnetcalculator'] = array('string');
+$meta['iphelper_tool1urltarget']   = array('multichoice','_choices' => array('_self','_blank','_parent','_top'));
+$meta['iphelper_tool1name'] = array('string');
+$meta['iphelper_tool1url'] = array('string');
+$meta['iphelper_tool2urltarget']   = array('multichoice','_choices' => array('_self','_blank','_parent','_top'));
+$meta['iphelper_tool2name'] = array('string');
+$meta['iphelper_tool2url'] = array('string');
+$meta['iphelper_tool3urltarget']   = array('multichoice','_choices' => array('_self','_blank','_parent','_top'));
+$meta['iphelper_tool3name'] = array('string');
+$meta['iphelper_tool3url'] = array('string');
+$meta['iphelper_tool4urltarget']   = array('multichoice','_choices' => array('_self','_blank','_parent','_top'));
+$meta['iphelper_tool4name'] = array('string');
+$meta['iphelper_tool4url'] = array('string');
+$meta['iphelper_tool5urltarget']   = array('multichoice','_choices' => array('_self','_blank','_parent','_top'));
+$meta['iphelper_tool5name'] = array('string');
+$meta['iphelper_tool5url'] = array('string');
+$meta['iphelper_tool6urltarget']   = array('multichoice','_choices' => array('_self','_blank','_parent','_top'));
+$meta['iphelper_tool6name'] = array('string');
+$meta['iphelper_tool6url'] = array('string');
+$meta['iphelper_tool7urltarget']   = array('multichoice','_choices' => array('_self','_blank','_parent','_top'));
+$meta['iphelper_tool7name'] = array('string');
+$meta['iphelper_tool7url'] = array('string');
+$meta['iphelper_tool8urltarget']   = array('multichoice','_choices' => array('_self','_blank','_parent','_top'));
+$meta['iphelper_tool8name'] = array('string');
+$meta['iphelper_tool8url'] = array('string');
+$meta['iphelper_tool9urltarget']   = array('multichoice','_choices' => array('_self','_blank','_parent','_top'));
+$meta['iphelper_tool9name'] = array('string');
+$meta['iphelper_tool9url'] = array('string');
+$meta['iphelper_tool10urltarget']   = array('multichoice','_choices' => array('_self','_blank','_parent','_top'));
+$meta['iphelper_tool10name'] = array('string');
+$meta['iphelper_tool10url'] = array('string');
+$meta['iphelper_rawtools'] = array('');
